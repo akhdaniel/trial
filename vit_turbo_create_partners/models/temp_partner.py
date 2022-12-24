43a088201b2f8fcc6b159333ac5bb015011eefd7
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2022-12-24 16:56:44 +0700

    Update temp_partner.py

--- vit_turbo_create_partners/models/temp_partner.py
+++ vit_turbo_create_partners/models/temp_partner.py
@@ -25,78 +25,8 @@ class temp_partner(models.Model):
 
     state = fields.Selection(string="State", selection=[('draft','Draft'),('imported','Imported')], default="draft")
 
-    # date = fields.Date(string="Date")
-    # title = fields.Char('Title')
-    # parent_name = fields.Char(string='Parent name')
-    # ref = fields.Char(string='Internal Reference', index=True)
-    #         # lang = fields.Selection(_lang_get, string='Language', default=lambda self: self.env.lang,
-    #         #                         help="All the emails and documents sent to this contact will be translated in this language.")
-    #         # tz = fields.Selection(_tz_get, string='Timezone', default=lambda self: self._context.get('tz'),
-    #         #                       help="The partner's timezone, used to output proper date and time values "
-    #         #                            "inside printed reports. It is important to set a value for this field. "
-    #         #                            "You should use the same timezone that is otherwise used to pick and "
-    #         #                            "render date and time values: your computer's timezone.")
-    #         # tz_offset = fields.Char(compute='_compute_tz_offset', string='Timezone offset', invisible=True)
-    # user_id = fields.Char(string='Salesperson',help='The internal user in charge of this contact.')
-    # vat = fields.Char(string='Tax ID', help="The Tax Identification Number. Complete it if the contact is subjected to government taxes. Used in some legal statements.")
 
-    # comment = fields.Text(string='Notes')
-
-    #         # category_id = fields.Many2many('res.partner.category', column1='partner_id',column2='category_id', string='Tags', default=_default_category)
-    #         # credit_limit = fields.Float(string='Credit Limit')
-    # barcode = fields.Char(oldname='ean13', help="Use a barcode to identify this contact from the Point of Sale.")
-    # active = fields.Boolean(default=True)
-    # customer = fields.Boolean(string='Is a Customer', default=True,
-    #                            help="Check this box if this contact is a customer. It can be selected in sales orders.")
-    # supplier = fields.Boolean(string='Is a Vendor',
-    #                            help="Check this box if this contact is a vendor. It can be selected in purchase orders.")
-    # employee = fields.Boolean(help="Check this box if this contact is an Employee.")
-    # function = fields.Char(string='Job Position')
-    # type = fields.Selection(
-    #     [('contact', 'Contact'),
-    #      ('invoice', 'Invoice address'),
-    #      ('delivery', 'Shipping address'),
-    #      ('other', 'Other address'),
-    #      ("private", "Private Address"),
-    #     ], string='Address Type',
-    #     default='contact',
-    #     help="Used by Sales and Purchase Apps to select the relevant address depending on the context.")
-    
-    # zip = fields.Char(string="ZIP")
-    # city = fields.Char(string="City")
-    # state_id = fields.Char(string="State")
-    # country_id = fields.Char(string='Country')
-    # is_company = fields.Boolean(string='Is a Company', default=False,help="Check if the contact is a company, otherwise it is a person")
-    # # industry_id = fields.Many2one('res.partner.industry', 'Industry')
-    # # company_type is only an interface field, do not use it in business logic
-    # company_type = fields.Selection(string='Company Type',
-    #     selection=[('person', 'Individual'), ('company', 'Company')],
-    #     )
-    # company_id = fields.Char('Company', )
-    # company_name = fields.Char('Company Name')
-    # technical field used for managing commercial fields
-    # commercial_partner_id = fields.Many2one('res.partner', compute='_compute_commercial_partner',
-    #                                          string='Commercial Entity', store=True, index=True)
-    # commercial_company_name = fields.Char('Company Name Entity', compute='_compute_commercial_company_name',
-    #                                       store=True)
-
-    # image: all image fields are base64 encoded and PIL-supported
-    # image = fields.Binary("Image", attachment=True,
-    #     help="This field holds the image used as avatar for this contact, limited to 1024x1024px",)
-    # image_medium = fields.Binary("Medium-sized image", attachment=True,
-    #     help="Medium-sized image of this contact. It is automatically "\
-    #          "resized as a 128x128px image, with aspect ratio preserved. "\
-    #          "Use this field in form views or some kanban views.")
-    # image_small = fields.Binary("Small-sized image", attachment=True,
-    #     help="Small-sized image of this contact. It is automatically "\
-    #          "resized as a 64x64px image, with aspect ratio preserved. "\
-    #          "Use this field anywhere a small image is required.")
-    # hack to allow using plain browse record in qweb views, and used in ir.qweb.field.contact
-    # self = fields.Many2one(comodel_name=_name, compute='_compute_get_ids')
-
-
-
-    @api.model_cr
+    # @api.model_cr
     def init(self):
         _logger.info("creating functions...")
         self.env.cr.execute("""DROP FUNCTION IF EXISTS vit_create_partners(TEXT);
@@ -261,7 +191,6 @@ class temp_partner(models.Model):
 
     def cron_import(self):
         self.process()
-        
         
     @api.multi
     def process(self):
